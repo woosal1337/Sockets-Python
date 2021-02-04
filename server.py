04cee3f8ce1813f874e03acf6883dba90e8aa8e3
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-02-04 20:10:28 +0300

    1.0

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+
+    xd = input("Write here:")
+    clientsocket.send(bytes(f"{xd}","utf-8"))
+    clientsocket.close()
